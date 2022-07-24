8d7aa25bb672f362b3fde8eeea455e09c90b95dc
Shunal Doke <mysticshunal@gmail.com>
2022-07-24 12:50:27 +0530

    added in-line message read. hid auth token.

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-#A Hulk Hogan quote spitter for discord#
+# A Hulk Hogan quote spitter for discord
 ![hulk hogan](https://www.gamespot.com/a/uploads/original/1578/15789737/3367450-hulkhogan.jpg)
-##TODO:##
+## TODO:
 - Read and output directly from file instead of loading into array
 - Add gif option
 - Scrape for more Hogan quotes rather than having to manually add them
-- Make Discord login token private... somehow
--- main.js
+++ main.js
@@ -4,23 +4,38 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 const readline = require("readline");
 const fs = require("fs");
 const { allowedNodeEnvironmentFlags } = require('process');
+const config = require('./config.json')
 
-const file = readline.createInterface({
-    input: fs.createReadStream('quotes.txt'),
-    output: process.stdout,
-    terminal: false
-})
+// const file = readline.createInterface({
+//     input: fs.createReadStream('quotes.txt'),
+//     output: process.stdout,
+//     terminal: false
+// })
 
 let quotes = fs.readFileSync('quotes.txt').toString().split("\n");
+const triggerWords = ['hogan pls', 'job'];
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on("message", msg => {
-    if (msg.content === "hogan pls") {
-        msg.reply(quotes[Math.floor(Math.random() * quotes.length)] + ', brother');
-    }
+    if(msg.author.bot) return false;
+
+    triggerWords.forEach((word) => {
+        if(msg.content.includes(word)) {
+            if(word === 'hogan pls') {
+                msg.reply(quotes[Math.floor(Math.random() * quotes.length)] + ', brother');
+            } else if(word === 'job') {
+                msg.reply("That doesn't work for me, brother.");
+            }
+        }
+    });
+
+    // if (msg.content === "hogan pls") {
+        
+    // } else if(msg.content === "job") {
+    // }
 });
 
-client.login('OTk3NDg1NTM4NTUyNTc4MTUw.G855jo.ESp6WTEeGaLm6fjgoRxMrR3y8qnFKB9PndDjlM')
+client.login(config.token)
