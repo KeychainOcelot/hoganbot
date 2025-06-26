131f88514e147a9aa5461877c12f6bcd5d7b87bf
Shunal Doke <mysticshunal@gmail.com>
2025-06-26 22:14:39 +0530

    so i've been rewriting hoganbot

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 //Dependencies
-const { Client, GatewayIntentBits } = require('discord.js');
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { REST, Routes } from 'discord.js';
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
-const fs = require("fs");
-const config = require('./config.json')
+import * as fs from 'node:fs';
+import * as config from './config.json';
 
 //Trigger words for the bot
 const triggerWords = ['hogan pls', 'job', 'brother', 'hogan gif'];
@@ -11,29 +12,60 @@ const triggerWords = ['hogan pls', 'job', 'brother', 'hogan gif'];
 let quotes = fs.readFileSync('quotes.txt').toString().split("\n");
 let gifs = fs.readFileSync('gifs.txt').toString().split("\n");
 
+//Slash commands
+const commands = [
+  {
+    name: 'brother',
+    description: 'You get tales of my escapades, brother',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(config.token);
+
+try {
+  console.log('Started refreshing application (/) commands.');
+
+  await rest.put(Routes.applicationCommands(Client), { body: commands });
+
+  console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+  console.error(error);
+}
+
 //Bot ready
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`)
+  console.log(`Logged in as ${client.user.tag}!`)
 })
 
-//Bot main logic
-client.on("message", msg => {
-    if(msg.author.bot) return false;
+//New attempt at bot logic (circa 2025)
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
 
-    triggerWords.forEach((word) => {
-        if(msg.content.includes(word)) {
-            if(word === 'hogan pls') {
-                msg.reply(quotes[Math.floor(Math.random() * quotes.length)] + ', brother');
-            } else if(word === 'job') {
-                msg.reply("That doesn't work for me, brother.");
-            } else if(word === 'hogan gif') {
-                msg.reply(gifs[Math.floor(Math.random() * gifs.length)]);
-            } else if(word === 'brother') {
-                msg.reply('...brother.');
-            }
-        }
-    });
-});
+  if (interaction.commandName === 'brother') {
+    await interaction.reply(quotes[Math.floor(Math.random() * quotes.length)] + ', brother');
+  }
+})
+
+
+
+//Bot main logic
+//client.on("message", msg => {
+//  if (msg.author.bot) return false;
+
+//  triggerWords.forEach((word) => {
+//    if (msg.content.includes(word)) {
+//      if (word === 'hogan pls') {
+//        msg.reply(quotes[Math.floor(Math.random() * quotes.length)] + ', brother');
+//      } else if (word === 'job') {
+//        msg.reply("That doesn't work for me, brother.");
+//      } else if (word === 'hogan gif') {
+//        msg.reply(gifs[Math.floor(Math.random() * gifs.length)]);
+//      } else if (word === 'brother') {
+//        msg.reply('...brother.');
+//      }
+//    }
+//  });
+//});
 
 //Bot config token
 client.login(config.token)
