0fbe209547aff2d41b1571432f079d3ad2e369ee
Shunal Doke <mysticshunal@gmail.com>
2025-08-02 00:09:20 +0530

    added quote suffix, but it doesn't take anything aside from 'dude'

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const triggerWords = ['hogan pls', 'job', 'brother', 'hogan gif'];
 //Bot responses
 let quotes = fs.readFileSync('quotes.txt').toString().split("\n");
 let gifs = fs.readFileSync('gifs.txt').toString().split("\n");
+const enders = ['... brother.', '... dude.', '... jack!', '... man.']
 
 //Bot client Instance
 const client = new Client({
@@ -32,20 +33,21 @@ client.on('messageCreate', message => {
 	const messageContent = message.content.toLowerCase();
 
 	for (const [index, trigger] of triggerWords.entries()) {
-        if (messageContent.includes(trigger)) {
-            
-            // Send GIF for 'hogan gif' trigger, quotes for remaining triggers
-            if (trigger === 'hogan gif') {
-                const gif = gifs[Math.floor(Math.random() * gifs.length)];
-                message.reply(gif);
-            } else {
-                const quote = quotes[Math.floor(Math.random() * quotes.length)];
-                message.reply(quote);
-            }
-            
-            return;
-        }
-    }
+		if (messageContent.includes(trigger)) {
+
+			// Send GIF for 'hogan gif' trigger, quotes for remaining triggers
+			if (trigger === 'hogan gif') {
+				const gif = gifs[Math.floor(Math.random() * gifs.length)];
+				message.reply(gif);
+			} else {
+				const quote = quotes[Math.floor(Math.random() * quotes.length)];
+				const ender = enders[Math.floor(Math.random() * enders.length)];
+				message.reply(quote + ender);
+			}
+
+			return;
+		}
+	}
 });
 
 //Bot token login
